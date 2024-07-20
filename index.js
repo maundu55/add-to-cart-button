4294d57c6b52c4b52edd4f3b252012f5bcca85ed
maundu55 <marymaundu.mm@gmail.com>
2024-07-20 10:43:54 +0300

    create an array

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const btnElement = document.querySelector('.cart-js');
 const cartElement = document.querySelector('.fa-cart-shopping');
 
 const items = [];
-let item;
+let cartQuantity = 0;
 
     btnElement.addEventListener('click', () =>{
         //console.log('clicked')
         items.push(btnElement);
         console.log(items);
     })
-    if(item === ){
-
-    }
+    items.forEach(item => {
+        if(item){
+            cartQuantity++;
+        }else{
+            cartQuantity += item;
+        }
+        console.log(cartQuantity)
+    })
+    
