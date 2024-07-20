07c029c59659263901dd9e01bf2a4b0c6be7b8a0
maundu55 <marymaundu.mm@gmail.com>
2024-07-20 10:34:29 +0300

    make button interactive

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const btnElement = document.querySelector('.cart-js');
 const cartElement = document.querySelector('.fa-cart-shopping');
 
+const items = [];
+let item;
+
+    btnElement.addEventListener('click', () =>{
+        //console.log('clicked')
+        items.push(btnElement);
+        console.log(items);
+    })
+    if(item === ){
+
+    }
